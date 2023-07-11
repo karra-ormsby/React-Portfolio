7e33bcac61ee5e2c54b9822fc8d83f92fb6350aa
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-07-11 13:05:01 +1000

    seperated code for the different pages into their own folder. Added a clickable link to download resume. Added some placeholders to start to build projects in porfolio page

---
+++ react-portfolio/src/components/pages/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+
+            <a href="https://drive.google.com/uc?id=1DlYiT_4sKheuU7KeLl6HKCg3ZPulpg0k&export=download" download>Click here</a>
+
+        </div>
+    );
+}
+
+export default Resume;
