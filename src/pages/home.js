03ce6fa6d1a83b061f22bafdb12158ecffb736fc
karra-ormsby <124136371+karra-ormsby@users.noreply.github.com>
2023-11-09 15:58:12 +1100

    adjusting header so that it is responsive

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,7 +6,7 @@ import Portfolio from "./Portfolio";
 import Resume from "./Resume";
 import Contact from "./Contact";
 import { useTheme } from "../utils/ThemeContext";
-import { Switch } from 'antd';
+import { Switch, Row, Col } from 'antd';
 import { GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 function Home() {
@@ -20,7 +20,7 @@ function Home() {
 
     return (
         <section id="home" className={`app ${darkTheme ? 'dark' : 'light'}`}>
-            <header className={`header ${darkTheme ? 'dark' : 'light'}`}>
+            {/* <header className={`header ${darkTheme ? 'dark' : 'light'}`}>
                 <h1 id='header-heading'> Karra Ormsby</h1>
                 <div id='nav-links'>
                     <HashLink smooth to='/React-Portfolio/#about'>About Me</HashLink>
@@ -42,7 +42,50 @@ function Home() {
                         <Switch checkedChildren="LIGHT ☀️" unCheckedChildren="DARK 🌙" defaultChecked onChange={toggleTheme} />
                     </div>
                 </div>
-            </header>
+            </header> */}
+            <header className={`header ${darkTheme ? 'dark' : 'light'}`}>
+                <h1 id='header-heading'>Karra Ormsby</h1>
+                <Row>
+                    <Col sm={1} lg={2}>
+                        <div></div>
+                    </Col>
+                    <Col sm={4} md={14} lg={10}>
+                        <div id='nav-links'>
+                            <Row justify="space-between">
+                                <Col span={5}>
+                                <HashLink smooth to='/React-Portfolio/#about'>About Me</HashLink>
+                                </Col>
+                                <Col span={5}>
+                                <HashLink smooth to='/React-Portfolio/#portfolio'>Portfolio</HashLink>
+                                </Col>
+                                <Col span={5}>
+                                <HashLink smooth to='/React-Portfolio/#resume'>Resume</HashLink>
+                                </Col>
+                                <Col span={5}>
+                                <HashLink smooth to='/React-Portfolio/#contact'>Contact Me</HashLink>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                    <Col sm={1} lg={2}>
+                        <div></div>
+                    </Col>
+                </Row>
+                <div className="other-header">
+                    <div></div>
+                    <div id='external-links'>
+                    <a href="https://github.com/karra-ormsby">
+                        <GithubOutlined />
+                    </a>
+                    <a href="https://au.linkedin.com/">
+                        <LinkedinOutlined />
+                    </a>
+                    </div>
+                    <div id='dark-theme-toggle'>
+                    <Switch checkedChildren="LIGHT ☀️" unCheckedChildren="DARK 🌙" defaultChecked onChange={toggleTheme} />
+                    </div>
+                </div>
+                </header>
             <About />
             <Portfolio />
             <Resume />
